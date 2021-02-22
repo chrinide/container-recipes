0c0f2487ca0ab392c5f666590883bd88ac09f4be
William Huhn <whuhn@anl.gov>
2021-02-22 18:21:45 -0500

    Add CentOS build for FHI-aims
    This build piggybacks off of the CentOS base image in the previous
    commit, but it has a number of issues:
    * currently requires that the CentOS base image be present in the
      CentOS base image folder,
    * Requires the FHI-aims git repo be present in a "FHIaims" folder
    * Exposes some bad elements in the CentOS base image; ScaLAPACK
      only exists statically, and the executable cannot find OpenBLAS
      at execution since it's in a non-standard folder.
    
    Removed the old build, since it's no longer relevant.

---
+++ FHI-aims/centos/initial_cache.cmake
@@ -0,0 +1,17 @@
+set(TARGET_NAME aims.x CACHE STRING "")
+set(CMAKE_INSTALL_PREFIX "/opt/FHIaims/" CACHE STRING "")
+
+###############
+# Basic Flags #
+###############
+set(CMAKE_Fortran_COMPILER mpifort CACHE STRING "")
+set(CMAKE_Fortran_FLAGS "-O2 -ffree-line-length-none" CACHE STRING "")
+set(Fortran_MIN_FLAGS "-O0 -ffree-line-length-none" CACHE STRING "")
+set(LIB_PATHS "/opt/scalapack/lib/;/opt/openblas/lib/" CACHE STRING "")
+set(LIBS "scalapack openblas" CACHE STRING "")
+
+###############
+# C,C++ Flags #
+###############
+set(CMAKE_C_COMPILER gcc CACHE STRING "")
+set(CMAKE_C_FLAGS "-O2 -DNDEBUG -funroll-loops" CACHE STRING "")
